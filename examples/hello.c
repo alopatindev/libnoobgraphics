0c7242592be9270a744c4eb5adbec5a2c42058f3
Alexander Lopatin <sbar.geek@gmail.com>
2013-04-15 05:40:54 +0400

    example update

--- examples/hello.c
+++ examples/hello.c
@@ -6,18 +6,14 @@ void on_update(int dt)
 
 void on_render()
 {
+    nb_set_color(0x00FF00FF);
+    nb_draw_square(30, 30, 400, 300);
+    nb_set_color(0xFF0000FF);
+    nb_draw_line(0, 0, 400, 300, 30);
 }
 
 int main()
 {
     nb_init_graphics(800, 600, "Hi, there!", on_update, on_render);
-    while (1)
-    {
-        nb_set_color(0x00FF00FF);
-        nb_draw_square(30, 30, 400, 300);
-        nb_set_color(0xFF0000FF);
-        nb_draw_line(0, 0, 400, 300, 30);
-    }
-    nb_destroy_graphics();
     return 0;
 }
